f66799174ff69a289cb24b493f58991736bb8c3a
Alexandre <alxrds@hotmail.com>
2022-05-25 22:32:38 -0300

    First commit

---
+++ src/main/java/com/alexandre/api/parking/repositories/ParkingSpotRepository.java
@@ -0,0 +1,15 @@
+package com.alexandre.api.parking.repositories;
+
+import com.alexandre.api.parking.models.ParkingSpotModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface ParkingSpotRepository extends JpaRepository<ParkingSpotModel, UUID> {
+
+    boolean existsByLicensePlateCar(String licensePlateCar);
+    boolean existsByParkingSpotNumber(String parkingSpotNumber);
+    boolean existsByApartmentAndBlock(String apartment, String block);
+}
